11a563bece5041fa03488ab965c2f12a7ab463c6
Ajinkya Borade <ajinkyab1987@gmail.com>
2018-08-21 22:19:28 +0530

    code refactoring WIP

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Config contains the configuration of the url shortener.
+type Config struct {
+	Server struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+	} `json:"server"`
+	Redis struct {
+		Host     string `json:"host"`
+		Password string `json:"password"`
+		DB       string `json:"db"`
+	} `json:"redis"`
+	Postgres struct {
+		Host     string `json:"host"`
+		Port     string `json:"port"`
+		User     string `json:"user"`
+		Password string `json:"password"`
+		DB       string `json:"db"`
+	} `json:"postgres"`
+	Options struct {
+		Prefix string `json:"prefix"`
+	} `json:"options"`
+}
+
+// FromFile returns a configuration parsed from the given file.
+func FromFile(path string) (*Config, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := json.Unmarshal(b, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/steelx/urlshortnr/config"
+	"github.com/steelx/urlshortnr/handlers"
+	"github.com/steelx/urlshortnr/storages"
+)
+
+func main() {
+	configPath := flag.String("config", "./config/config.json")
+	flag.Parse()
+	// Set use storage, select [Postgres, Filesystem, Redis ...]
+	storage := &storages.Postgres{}
+
+	// Read config
+	config, err := config.FromFile(*configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Init storage
+	if err = storage.Init(config); err != nil {
+		log.Fatal(err)
+	}
+
+	// Defers
+	defer storage.Close()
+
+	// Handlers
+	http.Handle("/encode/", handlers.EncodeHandler(config.Options.Prefix, storage))
+	http.Handle("/", handlers.RedirectHandler(storage))
+	http.Handle("/info/", handlers.DecodeHandler(storage))
+
+	// Wait signal
+	close := make(chan bool, 1)
+
+	// Create a server
+	server := &http.Server{Addr: fmt.Sprintf("%s:%s", config.Server.Host, config.Server.Port)}
+
+	// Check for a closing signal
+	go func() {
+		// Graceful shutdown
+		sigquit := make(chan os.Signal, 1)
+		signal.Notify(sigquit, os.Interrupt, os.Kill)
+
+		sig := <-sigquit
+		log.Printf("caught sig: %+v", sig)
+		log.Printf("Gracefully shutting down server...")
+
+		if err := server.Shutdown(nil); err != nil {
+			log.Printf("Unable to shut down server: %v", err)
+		} else {
+			log.Printf("Server stopped")
+		}
+	}()
+
+	// Start server
+	log.Printf("Starting HTTP Server. Listening at %q", server.Addr)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Println(err.Error())
+		} else {
+			log.Println("Server closed!")
+		}
+	}
+}
